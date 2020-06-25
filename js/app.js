bf13d8ad4a70e6a6bb4f96f1c143fa99350b6429
13194895822 <jingyang@iisjy.cn>
2020-06-25 17:48:09 +0800

    优化加载

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-// function getBlogTag(t_title, t_summary, t_img, t_link) {
-//   var s = `<div class="col-lg-4 col-md-6 col-sm-6" ><div class="blog-post"><div class="post-img"><img class="img-fluid" src="${t_img}" alt="image"></div><div class="blog-details"><a target="_blank" href="${t_link}"><h4 class="media-heading">${t_title}</h4></a><p>${t_summary}</p><a target="_blank" href="${t_link}" class="blog-post-link">阅读全文 <i class="fa fa-angle-double-right"></i></a></div></div></div>`;
-//   return s;
-// }
-// function getContactTag(t_name, t_link) {
-//   var s = `<div class="col-lg-6 col-sm-12"><div class="feature-wrap" ><div class="media"><div class="align-self-center mr-4"><div class="icon-wrap"><i class="ion ion-cube"></i></div></div><div class="media-body"><a target="_blank" href="${t_link}">${t_name}</a></div></div></div ></div >`;
-//   return s;
-// }
-// function getNavbarTag(t_link, t_title) {
-//   var s = `<li class="nav-item"><a class="nav-link" href = "${t_link}" >${t_title}</a ></li >`;
-//   return s;
-// }
-// function getProjectTag(t_name, t_imgpath, t_link) {
-//   var s = `<div class="col-lg-4 col-md-4 col-sm-6"><div class="product-wrap" ><div class="img-wrap"><img src="${t_imgpath}" alt="${t_name}" /><div class="product-overlay"><a target="_blank" href="${t_link}" class="icon-wrap"><i class="ion ion-android-arrow-forward"></i></a></div></div></div ></div >`;
-//   return s;
-// }
 const getBlogTag = (t_title, t_summary, t_img, t_link)=>`<div class="col-lg-4 col-md-6 col-sm-6" ><div class="blog-post"><div class="post-img"><img class="img-fluid" src="${t_img}" alt="image"></div><div class="blog-details"><a target="_blank" href="${t_link}"><h4 class="media-heading">${t_title}</h4></a><p>${t_summary}</p><a target="_blank" href="${t_link}" class="blog-post-link">阅读全文 <i class="fa fa-angle-double-right"></i></a></div></div></div>`;
 const getContactTag =(t_name, t_link)=>`<div class="col-lg-6 col-sm-12"><div class="feature-wrap" ><div class="media"><div class="align-self-center mr-4"><div class="icon-wrap"><i class="ion ion-cube"></i></div></div><div class="media-body"><a target="_blank" href="${t_link}">${t_name}</a></div></div></div ></div >`;
 const getNavbarTag =(t_link, t_title)=>`<li class="nav-item"><a class="nav-link" href = "${t_link}" >${t_title}</a ></li >`;
@@ -75,8 +59,18 @@ function getProject() {
   }
   document.getElementById("proj_list").innerHTML = l + s + r;
 }
+function getProject2(){
+  fetch("/json/projectlist.json")
+  .then(resp => resp.json())
+  .then(obj=>{
+    var s = '<div class="col-12"><div class="section-header" ><div class="sec-icon"><i class="ion ion-android-list"></i></div><div class="sec-title">Projects</div></div ></div >';
+    const r = '<div class="col-12"><div class="see-more" ><a target="_blank" href="#" class="btn btn-primary main-btn bg-main">View More</a></div ></div >';
+    obj.project_items.forEach(elem => s+=`<div class="col-lg-4 col-md-4 col-sm-6"><div class="product-wrap" ><div class="img-wrap"><img src="https://img-${elem.imgtype}-cdn.iisjy.cn/${elem.imghash}.${elem.imgtype}" alt="${elem.name}" /><div class="product-overlay"><a target="_blank" href="${elem.link}" class="icon-wrap"><i class="ion ion-android-arrow-forward"></i></a></div></div></div ></div >`);
+    document.getElementById("proj_list").innerHTML = (s + r);
+  })
+}
 function init_elements() {
-  getProject();
+  getProject2();
   getBlog();
   getContact();
 }
